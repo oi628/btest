88e7950b6ee5969ce01c91c73c1101f9966147a4
oi628 <85979003+oi628@users.noreply.github.com>
2023-02-25 21:04:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $('#mainButton').click(function(){
           $('#second-sentence').fadeIn(1000).delay(2000).fadeOut(1000, function() {
             $('#centered-div').fadeIn(1000);
             $('audio').get(0).play();
-			if (window.innerWidth<=576) {
+			if ((screen.width<=576  || window.innerWidth<=576)) {
 				$("body").css("background-image", "url('pic.jpeg')");
 			} else {
 				$("body").css("background-image", "url('wishB.jpg')");
